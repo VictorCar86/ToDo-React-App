e1ea83356ff658e5b053bb3b31d74f1c4d2a14b3
Victor Carreno <vmanuelcar@hotmail.com>
2022-09-23 23:24:06 -0500

    New location for GitHub

--- src/TodoContext/useLocalStorage.js
+++ src/TodoContext/useLocalStorage.js
@@ -1,48 +1,98 @@
 import React from "react";
 
+
 function useLocalStorage(itemName, initialValue){
-    const [error, setError] = React.useState(false);
-    const [loading, setLoading] = React.useState(true);
-    const [item, setItem] = React.useState(initialValue);
+  // last states used:
+  // const [error, setError] = React.useState(false);
+  // const [loading, setLoading] = React.useState(true);
+  // const [item, setItem] = React.useState(initialValue);
 
-    React.useEffect(() => {
-      setTimeout(() => {
-        try {
-          const localStorageItem = localStorage.getItem(itemName);
-          let parsedItem;
+  const [state, dispatch] = React.useReducer(reducer, initialState({ initialValue }))
 
-          if (!localStorageItem){
-            localStorage.setItem(itemName, JSON.stringify(initialValue))
-            parsedItem = initialValue
-          } else {
-            parsedItem = JSON.parse(localStorageItem)
-          }
+  const {
+    loading,
+    error,
+    item,
+  } = state;
 
-          setItem(parsedItem)
-          setLoading(false)
-        } catch (error){
-          setError(error)
-        }
-      }, 1000)
-    }, []);
+  // Action creators
+  const onError = (error) => dispatch({ type: actionTypes.error, payload: error});
+  const onSuccess = (parsedItem) => dispatch({ type: actionTypes.success, payload: parsedItem});
+  const onSave = (newItem) => dispatch({ type: actionTypes.save, payload: newItem});
 
-
-    const saveItem = newItems => {
+  React.useEffect(() => {
+    setTimeout(() => {
       try {
-        const stringifiedItem = JSON.stringify(newItems);
-        localStorage.setItem(itemName, stringifiedItem);
-        setItem(newItems);
-      } catch (error) {
-        setError(error)
-      }
-    }
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
 
-    return {
-      item,
-      saveItem,
-      loading,
-      error
-    };
+        if (!localStorageItem){
+          localStorage.setItem(itemName, JSON.stringify(initialValue))
+          parsedItem = initialValue
+        } else {
+          parsedItem = JSON.parse(localStorageItem)
+        }
+
+        onSuccess(parsedItem)
+      } catch (error){
+        onError(error)
+      }
+    }, 1000)
+  }, []);
+
+  const saveItem = newItems => {
+    try {
+      const stringifiedItem = JSON.stringify(newItems);
+      localStorage.setItem(itemName, stringifiedItem);
+      onSave(newItems);
+    } catch (error) {
+      onError(error)
+    }
+  }
+
+  return {
+    item,
+    saveItem,
+    loading,
+    error
+  };
+}
+
+const initialState = ({ initialValue }) => ({
+  // We use a function in order to extract the initial value from the function
+  loading: true,
+  error: false,
+  item: initialValue,
+});
+
+const actionTypes = {
+  error: 'ERROR',
+  success: 'SUCCESS',
+  save: 'SAVE',
+}
+
+const reducer = (state, action) => {
+  switch (action.type){
+    case actionTypes.error:
+      return {
+        ...state,
+        error: true,
+      }
+    case actionTypes.success:
+      return {
+        ...state,
+        error:false,
+        loading: false,
+        item: action.payload,
+      }
+    case actionTypes.save:
+      return {
+        ...state,
+        item: action.payload
+      }
+    default:
+      return state
+  }
 }
 
 export { useLocalStorage };
